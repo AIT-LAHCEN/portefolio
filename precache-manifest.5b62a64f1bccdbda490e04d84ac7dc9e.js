4d25c60437ac6786c19b0110696535a033cda899
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-03-26 14:02:23 +0100

    Updates

---
+++ precache-manifest.5b62a64f1bccdbda490e04d84ac7dc9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68849608887aab9d3d47f036659877ec",
+    "url": "/portefolio/index.html"
+  },
+  {
+    "revision": "9e946824e8f17553bce5",
+    "url": "/portefolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "eb0ccb8c09ba30244e29",
+    "url": "/portefolio/static/js/2.3a0715fb.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "/portefolio/static/js/2.3a0715fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e946824e8f17553bce5",
+    "url": "/portefolio/static/js/main.6eb5b251.chunk.js"
+  },
+  {
+    "revision": "0232ab2161557ffd954e",
+    "url": "/portefolio/static/js/runtime-main.2fe7ea77.js"
+  }
+]);
