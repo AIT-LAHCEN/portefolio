b6ce62fd4183abeb5d0eef718c951f50927d3b1f
AIT-LAHCEN <ah.aitlahcen@gmail.com>
2020-03-26 18:55:47 +0100

    fixing some urls

--- src/Components/About.js
+++ src/Components/About.js
@@ -39,15 +39,13 @@ class About extends Component {
 					   </p>
                </div>
                <div className="columns download">
-                     <p><form method="get" action="_Documents/CV.pdf">
-                        <button type="submit">Télecharger</button>
-                     </form>
-                     <a href="https://ait-lahcen.github.io/portefolio/_Documents/CV.pdf" className="button"><i className="fa fa-download"></i>Visualiser Mon CV</a>
-                     <a href="_Documents/Lettre_de_motivation.pdf" className="button"><i className="fa fa-download"></i>La Lettre de Motivation</a>
-                     <a href="_Documents/projet_professionnel.pdf" className="button"><i className="fa fa-download"></i>Le Projet Professionnel</a>
-                     <a href="_Documents/fiche_metier.pdf" className="button"><i className="fa fa-download"></i>La Fiche Métier</a>
-                     <a href="_Documents/carte_identite_entreprise.pdf" className="button"><i className="fa fa-download"></i>La Carte D'identité des entreprises</a>
-                     <a href="_Documents/bilan_personnel.pdf" className="button"><i className="fa fa-download"></i>Le Bilan Personnel</a>
+                  <p>
+                     <a href="https://drive.google.com/open?id=1l2mBL-Q6CpTBO6wTjffBP081Za6zuvcq" className="button"><i className="fa fa-download"></i>Visualiser Mon CV</a>
+                     <a href="https://drive.google.com/open?id=12x_Dp-9vCXPZL_OKbEa6e3oIv2fUR749" className="button"><i className="fa fa-download"></i>La Lettre de Motivation</a>
+                     <a href="https://drive.google.com/open?id=1MygL_r0VmgwUnnwT7CA3pDc63kAZDcsm" className="button"><i className="fa fa-download"></i>Le Projet Professionnel</a>
+                     <a href="https://drive.google.com/open?id=17PaAfJBd31fZgg9DQRruGIqKnp5oIvDB" className="button"><i className="fa fa-download"></i>La Fiche Métier</a>
+                     <a href="https://drive.google.com/open?id=1AT9r36WBjBQueOdnuKQTqE3srKqadlLz" className="button"><i className="fa fa-download"></i>La Carte D'identité des entreprises</a>
+                     <a href="https://drive.google.com/open?id=15Zpf7PHdobqyv2W3n7XQj73_SCbaOOrk" className="button"><i className="fa fa-download"></i>Le Bilan Personnel</a>
                   </p>
                </div>
             </div>
